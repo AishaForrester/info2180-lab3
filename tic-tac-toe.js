433d2bdbfd441e45de05a97238a1064b57988162
Aisha <Aisha.Forrester@gmail.com>
2023-10-24 21:34:14 -0500

    Completed Section Two

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var squares = document.querySelectorAll("#board div")
-  squares.forEach(function(square) {
-    square.classList.add("square")
-  });
-  console.log(squares)  //testing
-  let boxes = Array.from(document.getElementsByClassName("square"))
-  console.log(boxes)  //testing
+document.addEventListener("DOMContentLoaded", function() {  //works when page is opened
+  let keepTrack = ["", "", "", "", "", "", "", "", ""]
+  const OT = "O"
+  const XT = "X"
+  playingNow = XT
 
+  var squares = document.querySelectorAll("#board div") //observing all div in board
+  squares.forEach(function(square, index) {
+    square.classList.add("square")
+
+    square.addEventListener("click", function(){  //works when space is clicked
+      if(keepTrack[index] == "") {
+        square.innerHTML = playingNow;
+
+        keepTrack[index] = playingNow  //updating to current play
+        square.classList.add(playingNow)  //ensuring that we are adding X or O to square
+
+        playingNow = playingNow == XT ? OT : XT //if X has been played, we navigate to O 
+      }
+      
+    });  //end of listener (for when button is clicked)
+    
+  }); //end of forEach for going through tiles
+  console.log(keepTrack)
+
+  
 });
  
 
